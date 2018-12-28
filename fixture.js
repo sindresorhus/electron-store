db6b32efad291eda647524253b955cf6eea45e1f
Brandon Smith <brandon93s@gmail.com>
2018-12-28 18:40:18 -0500

    Cleanup linting

--- fixture.js
+++ fixture.js
@@ -6,16 +6,16 @@ const Store = require('.');
 // Prevent Electron from never exiting when an exception happens
 process.on('uncaughtException', err => {
 	console.error('Exception:', err);
-	process.exit(1); // eslint-disable-line
+	process.exit(1);
 });
 
 const store = new Store({name: 'electron-store'});
 
 store.set('unicorn', '🦄');
-assert.equal(store.get('unicorn'), '🦄');
+assert.strictEqual(store.get('unicorn'), '🦄');
 
 store.delete('unicorn');
-assert.equal(store.get('unicorn'), undefined);
+assert.strictEqual(store.get('unicorn'), undefined);
 
 // To be checked in AVA
 store.set('ava', '🚀');
